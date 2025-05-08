138f18ffef98e4514e1d3aea181e3f7284bd6710
Jonathan Villegas <jvillegas86@gmail.com>
2025-05-07 23:04:59 -0300

    lastmodified changes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -9,3 +9,8 @@ const today = new Date();
 
 
 currentyear.innerHTML = ` <span class="highlight">${today.getFullYear()}</span> `;
+
+//last modified 
+
+let lastmodified = new Date(document.lastModified);
+document.getElementById('lastmodified').textContent = lastmodified.toLocaleString();
