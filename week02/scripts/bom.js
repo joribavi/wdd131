7e9b907b6b4deb6f4d68a2a6b5154c1b6ceff668
Jonathan Villegas <jvillegas86@gmail.com>
2025-05-15 18:00:18 -0300

    last changes for learing activity with js

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,40 @@
 const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
-const list = document.querySelector("li");
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
-li.textContent = input.value;
-deleteButton.textContent = "X";
-li.append(deleteButton);
-list.append(li);
+const list = document.querySelector("#list");
+
+
+button.addEventListener("click", function () {
+
+    if (input.value.trim() !== "") {
+        const li = document.createElement("li");
+        li.textContent = input.value;
+
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "X";
+        deleteButton.style.color = "red";
+        deleteButton.style.fontStyle = "bold";
+        li.append(deleteButton);
+        list.appendChild(li);
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+
+        })
+
+
+
+    }
+
+});
+
+
+
+
+
+
+input.value = '';
+input.focus();
+
+
+
