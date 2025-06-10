7820aae6058ff6cb08117a9bc15ac941feb12745
Jonathan Villegas <jvillegas86@gmail.com>
2025-06-09 22:05:51 -0300

    advancing with some js css changes

--- project/project.js
+++ project/project.js
@@ -40,3 +40,68 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
 
 }
+
+
+//tours section 
+
+const tours = document.querySelector('.tours-container');
+
+const card = document.createElement("div");
+
+card.classList.add("tour-card");
+
+tours.appendChild(card);
+
+card.classList.add("card-container")
+
+
+const imgClass = document.createElement("div");
+
+imgClass.classList.add("img");
+
+card.appendChild(imgClass);
+
+const img1 = document.createElement("img");
+
+img1.src = "images/tours-1.jpg";
+img1.alt = "cusco-tours";
+img1.loading = "lazy";
+
+imgClass.appendChild(img1);
+
+const cardContent = document.createElement("div");
+
+cardContent.classList.add("card-content");
+
+card.appendChild(cardContent);
+
+const cardTittle = document.createElement("h2");
+cardTittle.classList.add("tittle");
+
+cardContent.appendChild(cardTittle);
+
+cardTittle.textContent = "Find the Best Tours";
+
+const cardParagraph = document.createElement("p");
+
+cardParagraph.classList.add("description");
+
+cardContent.appendChild(cardParagraph);
+
+cardParagraph.textContent = "This is the place where Culture, History and Adventure meet";
+
+
+const Link = document.createElement("a");
+
+cardContent.appendChild(Link);
+
+Link.href = "#";
+
+Link.textContent = "Read More";
+
+
+
+
+
+
+
